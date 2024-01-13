6a0a55809222b646538e8ccd5d18cc0ae6910e33
Sergey Varlamov <msisdn.com@gmail.com>
2024-01-13 15:48:04 -0600

    Added all-mountain-skis, racing-skis, freeride-skis, freestyle-skis pages

--- app/freeride-skis/page.js
+++ app/freeride-skis/page.js
@@ -1,9 +1,56 @@
+"use client";
+import { useEffect, useState } from "react";
 import Banner from "@/components/layout/banner";
+import { getAllProducts } from "@/lib/actions/ski-actions";
+import ProductGrid from "@/components/product-grid";
+import Sort from "@/components/sort";
 
-export default function Freeride() {
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export default function Freeride({ searchParams }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, [searchParams]);
+
+  const fetchProducts = async () => {
+    try {
+      const { skis } = await getAllProducts({
+        ...searchParams,
+        category: "Freeride",
+      });
+      setProducts(skis);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <main>
       <Banner bannerName="4" />
+      <Container sx={{ pb: 6 }} maxWidth="xl">
+        <Box sx={{ flexGrow: 1 }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              pb: 2,
+            }}
+          >
+            <Typography variant="h5" align="center">
+              {products.length} Items
+            </Typography>
+            <Box>
+              <Sort />
+            </Box>
+          </Box>
+          <ProductGrid products={products} />
+        </Box>
+      </Container>
     </main>
   );
 }
